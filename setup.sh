4250b10eb6798301305a4c91d57f37a5ddfbf9b1
joonarafael <joona.kettunen21@gmail.com>
2024-03-27 11:06:51 +0200

    change pip install command

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 echo "Initializing program environment and installing dependencies..."
 sudo apt-get update
-sudo apt-get install python3.6 
-python get-pip.py
+sudo apt-get install python3.6 python3-pip
 python3 -m pip install beautifulsoup4 requests schedule
 chmod u+x ./run.sh
 chmod u+x ./delhistory.sh
